907d1502d04d69700a4ed5b2ac6baae967304113
nayellyq5 <nayellyq5@gmail.com>
2019-03-19 22:19:55 +0000

    Day one

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+var giphy_api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC"
 
 $("#search-button").click(function(){
-  
-  
-  
+$.ajax({
+        url: giphy_api_url ,
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+            var originalImage = response.data[0].images.fixed_width.url;
+      $('body').append('<img src ="' + originalImage + '"/>');
+
+        }
+    });
+
+
 });
 

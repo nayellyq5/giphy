9b35eb05c65b6f6b9763a84f8d907e9dd94bcd9a
nayellyq5 <nayellyq5@gmail.com>
2019-04-09 22:37:26 +0000

    last Day

--- js/script.js
+++ js/script.js
@@ -5,17 +5,19 @@
 
 $("#search-button").click(function(){
 
-var giphy_api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC"
-var searchTerm = $('input').val();
-var userUrl = "https://api.giphy.com/v1/gifs/search?q" + searchTerm + "=puppy&rating=pg&api_key=dc6zaTOxFJmzC"
+    var giphy_api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+    var searchTerm = $('input').val();
+    var userUrl = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
+
 
 $.ajax({
-        url: giphy_api_url = userUrl,
-        method: "GET",
-        success: function(response) {
-            // YOUR CODE GOES HERE
-            var originalImage = response.data[0].images.fixed_width.url;
-      $('body').append('<img src ="' + originalImage + '"/>');
+    url: giphy_api_url = userUrl,
+    method: "GET",
+    success: function(response) {
+        var randomImage = Math.random() * response.data.length
+        var imageIndex = Math.floor(randomImage);
+        var originalImage = response.data[imageIndex].images.fixed_width.url;
+        $('#display').html('<img src ="' + originalImage + '"/>');
 
         }
     });
